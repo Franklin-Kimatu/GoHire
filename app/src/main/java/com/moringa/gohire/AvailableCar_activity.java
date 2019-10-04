ed7702506c566da5d15abb1b064642dba5eecd49
Franklin-Kimatu <frankimatu@gmail.com>
2019-10-05 01:35:15 +0300

    add the butterknife use and the Binding of view

--- app/src/main/java/com/moringa/gohire/AvailableCar_activity.java
+++ app/src/main/java/com/moringa/gohire/AvailableCar_activity.java
@@ -1,15 +1,25 @@
 package com.moringa.gohire;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class AvailableCar_activity extends AppCompatActivity {
+    @BindView(R.id.carModelsAvailableId) TextView mCarAvailableModel;
+    @BindView(R.id.yearOfReleaseId) TextView mYearOfRealese;
+
+    private String[] carsAvailable =getResources().getStringArray(R.array.carmodels);
+    private String[] yearOfRelease =getResources().getStringArray(R.array.caryearofrelease);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_availablecar);
+        ButterKnife.bind(this);
     }
 }
