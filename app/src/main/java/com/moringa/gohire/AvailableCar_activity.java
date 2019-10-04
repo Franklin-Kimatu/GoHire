ad421c5668100fca66ab57315b856ac9dbb2a5ba
Franklin-Kimatu <frankimatu@gmail.com>
2019-10-04 23:19:50 +0300

    add the card view dependancy

---
+++ app/src/main/java/com/moringa/gohire/AvailableCar_activity.java
@@ -0,0 +1,15 @@
+package com.moringa.gohire;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AvailableCar_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_availablecar);
+    }
+}
