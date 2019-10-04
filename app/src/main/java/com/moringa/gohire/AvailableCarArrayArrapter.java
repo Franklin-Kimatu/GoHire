07ac06c71a9d60d952505c072d9f4a050c2b9c91
Franklin-Kimatu <frankimatu@gmail.com>
2019-10-05 01:03:09 +0300

    add the details for the arrayadapter

--- app/src/main/java/com/moringa/gohire/AvailableCarArrayArrapter.java
+++ app/src/main/java/com/moringa/gohire/AvailableCarArrayArrapter.java
@@ -1,4 +1,34 @@
 package com.moringa.gohire;
 
-public class AvailableCarArrayArrapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+
+
+
+public class AvailableCarArrayArrapter extends ArrayAdapter {
+    public AvailableCarArrayArrapter( Context context, int resource, Context mContext, String[] mAvailableCarModels, String[] mYearOfRelease) {
+        super(context, resource);
+        this.mContext = mContext;
+        this.mAvailableCarModels = mAvailableCarModels;
+        this.mYearOfRelease = mYearOfRelease;
+    }
+
+
+    @Override
+    public int getCount() {
+        return super.getCount();
+    }
+
+
+    @Override
+    public Object getItem(int position) {
+        String availablecar =mAvailableCarModels[position];
+        String realeseYear =mYearOfRelease[position];
+        return String.format("%s \n Year of release: %s",availablecar,realeseYear);
+    }
+
+    private Context mContext;
+    private String[] mAvailableCarModels;
+    private String[] mYearOfRelease;
 }
