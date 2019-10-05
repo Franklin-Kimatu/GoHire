a4f40c085275ae32ebc6b2663044223da5ae2d4e
Franklin-Kimatu <frankimatu@gmail.com>
2019-10-05 22:29:40 +0300

    add the correct string array to the activity to be displayed.

--- app/src/main/java/com/moringa/gohire/AvailableCar_activity.java
+++ app/src/main/java/com/moringa/gohire/AvailableCar_activity.java
@@ -1,6 +1,7 @@
 package com.moringa.gohire;
 
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
 
 
@@ -10,16 +11,19 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class AvailableCar_activity extends AppCompatActivity {
-    @BindView(R.id.carModelsAvailableId) TextView mCarAvailableModel;
-    @BindView(R.id.yearOfReleaseId) TextView mYearOfRealese;
+   @BindView(R.id.listView) ListView mListView;
 
-    private String[] carsAvailable =getResources().getStringArray(R.array.carmodels);
-    private String[] yearOfRelease =getResources().getStringArray(R.array.caryearofrelease);
+    private String[] carsAvailable =new String[]{"Ranger rover","Toyota V8","Mercedes C220","BMW X5","Mazda CX5","Mazda Axela","Subaru Forester","Subaru Legacy","Subaru Outback"};
+
+    private String[] yearOfRelease =new String[]{"2019","2012","2012","2010","2015","2016","2017","2012","2015"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_availablecar);
         ButterKnife.bind(this);
+
+        AvailableCarArrayArrapter adapter =new AvailableCarArrayArrapter(this,android.R.layout.simple_list_item_1,carsAvailable,yearOfRelease);
+        mListView.setAdapter(adapter);
     }
 }
