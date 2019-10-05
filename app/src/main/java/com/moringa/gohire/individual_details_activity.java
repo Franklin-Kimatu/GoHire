f41c0aff385e7887fec11e96384166b1b0ccdfaf
Franklin-Kimatu <frankimatu@gmail.com>
2019-10-05 23:53:30 +0300

    add the file to show the details for the individual vehicle

--- app/src/main/java/com/moringa/gohire/individual_details_activity.java
+++ app/src/main/java/com/moringa/gohire/individual_details_activity.java
@@ -1,14 +1,18 @@
 package com.moringa.gohire;
 
 import android.os.Bundle;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
+
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
 import com.moringa.gohire.ui.main.SectionsPagerAdapter;
 
 public class individual_details_activity extends AppCompatActivity {
