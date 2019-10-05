176674422bdf734e0e40dbd58eb754adb9124b72
Franklin-Kimatu <frankimatu@gmail.com>
2019-10-05 22:30:21 +0300

    add the bindview for different views

--- app/src/main/java/com/moringa/gohire/MainActivity.java
+++ app/src/main/java/com/moringa/gohire/MainActivity.java
@@ -2,13 +2,32 @@ package com.moringa.gohire;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.viewAvailableCarsButton) Button mViewAvailableCarsButton;
+    @BindView(R.id.textViewHeader) TextView mTextViewHeader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mViewAvailableCarsButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==mViewAvailableCarsButton){
+            Intent intent= new Intent(MainActivity.this,AvailableCar_activity.class);
+            startActivity(intent);
+        }
     }
 }
