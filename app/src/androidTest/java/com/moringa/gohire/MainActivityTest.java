3a4948c5332ac5a20120e06453605e310cef1a54
Franklin-Kimatu <frankimatu@gmail.com>
2019-10-07 11:22:51 +0300

    add the test for the names entered by the user.

--- app/src/androidTest/java/com/moringa/gohire/MainActivityTest.java
+++ app/src/androidTest/java/com/moringa/gohire/MainActivityTest.java
@@ -1,4 +1,30 @@
 package com.moringa.gohire;
 
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+//import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity>activityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void validateEditText(){
+        onView(withId(R.id.nameEditText)).perform(typeText("Frank"))
+                .check(matches(withText("Frank")));
+    }
 }
