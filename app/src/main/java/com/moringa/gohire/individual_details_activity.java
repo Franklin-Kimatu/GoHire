867a58d9725e75c4914a1aa4c6e6684c7dbcbc86
Franklin-Kimatu <frankimatu@gmail.com>
2019-10-11 15:07:12 +0300

    prepare the APi slot

--- app/src/main/java/com/moringa/gohire/individual_details_activity.java
+++ app/src/main/java/com/moringa/gohire/individual_details_activity.java
@@ -21,12 +21,6 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class individual_details_activity extends AppCompatActivity {
-    private String[] carsAvailable =new String[]{"Ranger rover","Toyota V8","Mercedes C220","BMW X5","Mazda CX5","Mazda Axela","Subaru Forester","Subaru Legacy","Subaru Outback"};
-    private String[] insuranceCover =new String[]{"Comprehensive","Comprehensive","Comprehensive","Comprehensive","Comprehensive","Third party and theft","Comprehensive","Third party only","Comprehensive","Third party"};
-    private String[] vehicleCondtion = new String[]{"Good","Good","Good","Good","Okay","Good","Okay","Good","Good","Good"};
-    private String[] availableDriver =new String[]{"Yes","Yes","Yes","Yes","Yes","Yes","Yes","Yes","Yes","Yes"};
-    private String[] amountPerDay=new String[]{"Kshs.100,000/day","Kshs.80,000/day","Kshs.70,000/day","Kshs.30,000/day","Kshs.10,000/day","Kshs.10,000/day","Kshs.15,000/day","Kshs.25,000/day","Kshs.9,000/day","Kshs.10,000/day"};
-    private int i;
 
 //    @BindView(R.id.textViewModel) TextView mCarModel;
 //    @BindView(R.id.textViewInsurance) TextView mInsuranceCover;
