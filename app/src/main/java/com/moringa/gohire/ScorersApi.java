eeb7c5a0db5b313a86e4efe7e24a8bc69a7ed8d7
Franklin-Kimatu <frankimatu@gmail.com>
2019-10-16 23:57:48 +0300

    change the call class in  the path being used

--- app/src/main/java/com/moringa/gohire/ScorersApi.java
+++ app/src/main/java/com/moringa/gohire/ScorersApi.java
@@ -1,10 +1,10 @@
 package com.moringa.gohire;
 
+import com.moringa.gohire.models.ScorersResponse;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Part;
 import retrofit2.http.Path;
-import retrofit2.http.Query;
 
 public interface ScorersApi {
 
