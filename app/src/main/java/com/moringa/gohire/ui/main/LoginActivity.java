9f53991ee3e2a89b18d0af5d81a3a508e2ac46ca
Franklin-Kimatu <frankimatu@gmail.com>
2019-10-19 15:20:36 +0300

    bind the views

--- app/src/main/java/com/moringa/gohire/ui/main/LoginActivity.java
+++ app/src/main/java/com/moringa/gohire/ui/main/LoginActivity.java
@@ -3,14 +3,31 @@ package com.moringa.gohire.ui.main;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.moringa.gohire.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.logoImage) ImageView mLogoImage;
+    @BindView(R.id.nameEditText) EditText mNameEditText;
+    @BindView(R.id.emailEditText) EditText mEmailEditText;
+    @BindView(R.id.passwordEditText) EditText mPasswordEditText;
+    @BindView(R.id.loginButton) Button mLoginButton;
+    @BindView(R.id.noAccountTextView) TextView mNoAccountTextView;
+    @BindView(R.id.signUpButton) Button mSignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
     }
 }
