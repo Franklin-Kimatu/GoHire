9e54195294cf1437a9df4505f29d7ded9935b4d1
Franklin-Kimatu <frankimatu@gmail.com>
2019-10-19 16:35:40 +0300

    add the name input manifest file.

--- app/src/main/java/com/moringa/gohire/ui/main/LoginActivity.java
+++ app/src/main/java/com/moringa/gohire/ui/main/LoginActivity.java
@@ -1,13 +1,23 @@
 package com.moringa.gohire.ui.main;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.firebase.client.Firebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.moringa.gohire.R;
 
 import butterknife.BindView;
@@ -22,6 +32,8 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.loginButton) Button mLoginButton;
     @BindView(R.id.noAccountTextView) TextView mNoAccountTextView;
     @BindView(R.id.signUpButton) Button mSignUpButton;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +41,54 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() !=null){
+
+                    startActivity(new Intent(LoginActivity.this,Main1Activity.class));
+
+                }
+            }
+        };
+
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSign();
+            }
+        });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    private void startSign(){
+        String email = mEmailEditText.getText().toString();
+        String password = mPasswordEditText.getText().toString();
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)){
+            Toast.makeText(LoginActivity.this,"Field are empty",Toast.LENGTH_SHORT).show();
+        }else{
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (!task.isSuccessful()){
+                        Toast.makeText(LoginActivity.this,"Sign in problem",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
+    }
+
+
 }
