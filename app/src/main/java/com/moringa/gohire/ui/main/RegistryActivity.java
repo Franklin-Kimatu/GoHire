24d9f4ab38bce285dc60cb43f1b4d131517793bb
Franklin-Kimatu <frankimatu@gmail.com>
2019-10-20 06:59:57 +0300

    add progress dialog in the login activity.

---
+++ app/src/main/java/com/moringa/gohire/ui/main/RegistryActivity.java
@@ -0,0 +1,13 @@
+package com.moringa.gohire.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registry);
+    }
+}
