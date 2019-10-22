1a9dba19643b495231b3d2af6efee7388f431b47
Franklin-Kimatu <frankimatu@gmail.com>
2019-10-22 16:38:41 +0300

    remove the comented areas.

--- app/src/main/java/com/moringa/gohire/adapters/ScorersPagerAdapter.java
+++ app/src/main/java/com/moringa/gohire/adapters/ScorersPagerAdapter.java
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
-import com.moringa.gohire.models.Player;
+
 import com.moringa.gohire.models.Scorer;
 import com.moringa.gohire.ui.main.BlankFragment;
 
